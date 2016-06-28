c5443633f7384ef2479061ddc1d6ac2349b08732
汪安军 <wanganjun1992@hotmail.com>
2016-06-28 19:26:12 +0800

    little change

--- routes/index.js
+++ routes/index.js
@@ -127,7 +127,7 @@ router.get("/chickens_at_hour", function(req, res){
 
     var currentHour = new Date(year, month, day, hour);
     var nextHour = new  Date(year, month, day, hour + 1);
-    console.log("GET chickens from " + req.query + " to " + nextHour);
+    console.log("GET chickens from " + currentHour + " to " + nextHour);
 
     Chicken
         .find()
